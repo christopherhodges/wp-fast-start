be89bbf1275e6d2838e02d24832173e9abe32e84
Benjamin Mosnier <bm@kappuccino.org>
2015-06-24 08:41:17 +0200

    WP 4.2.2
    Signed-off-by: Benjamin Mosnier <bm@kappuccino.org>

--- wp-config.php
+++ wp-config.php
@@ -6,7 +6,7 @@ include __DIR__.'/kappuccino.php';
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
  * Secret Keys, and ABSPATH. You can find more information by visiting
- * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
  * Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
